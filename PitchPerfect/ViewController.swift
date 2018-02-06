5c236f1437702fee968da9ce47d475f37a967790
Boubakar Mammar <boubakarmammar@gmail.com>
2018-02-06 11:36:57 +0100

    Lesson3 ViewController and Multipe Views

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabal: UILabel!
+    @IBOutlet weak var recordingButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillApear called")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("button preseed")
+        recordingLabal.text = "Recording in Progress"
+        recordingButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordingLabal.text = "Tab to Record"
+        recordingButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+    }
 }
 
